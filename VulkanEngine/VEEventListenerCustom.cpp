c86126466dfdd9e6af09a5e49e11a053760eb459
Yevhen Baidiuk <y.baidiuk@gmail.com>
2021-03-15 18:17:30 +0100

    task 1 fix bug.

--- VulkanEngine/VEEventListenerCustom.cpp
+++ VulkanEngine/VEEventListenerCustom.cpp
@@ -10,28 +10,36 @@
 namespace ve {
 
     void VEEventListenerCustom::onFrameEnded(veEvent event) {
-		if (event.dt > 0.2) {
 
-			VkExtent2D extent = getWindowPointer()->getExtent();
-			uint32_t imageSize = extent.width * extent.height * 4;
-			VkImage image = getRendererPointer()->getSwapChainImage();
+        m_time_sum += (float) event.dt;
+        if (m_time_sum >= 1) {
+            m_screenshot_current_second = 0;
+            m_time_sum = 0;
+        }
 
-			auto *dataImage = new uint8_t[imageSize];
+        if (m_screenshot_current_second < m_max_screenshot_per_sec) {
 
-			vh::vhBufCopySwapChainImageToHost(getRendererPointer()->getDevice(),
-				getRendererPointer()->getVmaAllocator(),
-				getRendererPointer()->getGraphicsQueue(),
-				getRendererPointer()->getCommandPool(),
-				image, VK_FORMAT_R8G8B8A8_UNORM,
-				VK_IMAGE_ASPECT_COLOR_BIT, VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL,
-				dataImage, extent.width, extent.height, imageSize);
+            VkExtent2D extent = getWindowPointer()->getExtent();
+            uint32_t imageSize = extent.width * extent.height * 4;
+            VkImage image = getRendererPointer()->getSwapChainImage();
 
-			m_numScreenshot++;
+            auto *dataImage = new uint8_t[imageSize];
 
-			std::string name("media/screenshots/screenshot" + std::to_string(m_numScreenshot-1) + ".jpg");
-			stbi_write_jpg(name.c_str(), extent.width, extent.height, 4, dataImage, 4 * extent.width);
-			delete[] dataImage;
-		}
+            vh::vhBufCopySwapChainImageToHost(getRendererPointer()->getDevice(),
+                                              getRendererPointer()->getVmaAllocator(),
+                                              getRendererPointer()->getGraphicsQueue(),
+                                              getRendererPointer()->getCommandPool(),
+                                              image, VK_FORMAT_R8G8B8A8_UNORM,
+                                              VK_IMAGE_ASPECT_COLOR_BIT, VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL,
+                                              dataImage, extent.width, extent.height, imageSize);
+
+            m_numScreenshot++;
+            m_screenshot_current_second++;
+
+            std::string name("media/screenshots/screenshot" + std::to_string(m_numScreenshot - 1) + ".jpg");
+            stbi_write_jpg(name.c_str(), extent.width, extent.height, 4, dataImage, 4 * extent.width);
+            delete[] dataImage;
+        }
     }
 
 
--- VulkanEngine/VEEventListenerCustom.h
+++ VulkanEngine/VEEventListenerCustom.h
@@ -13,37 +13,41 @@
 
 namespace ve {
 
-	/**
-	*
-	* \brief An event listener for GLFW events
-	*
-	* This is the default event listener for GLFW events. Basically it steers the standard camera around,
-	* just as you would expect from a first person shooter.
-	*
-	*/
-	class VEEventListenerCustom : public VEEventListener {
+    /**
+    *
+    * \brief An event listener for GLFW events
+    *
+    * This is the default event listener for GLFW events. Basically it steers the standard camera around,
+    * just as you would expect from a first person shooter.
+    *
+    */
+    class VEEventListenerCustom : public VEEventListener {
 
-	protected:
+    protected:
 //		bool  m_usePrevCursorPosition = false;			///<Can I use the previous cursor position for moving the camera?
 //		bool  m_rightButtonClicked = false;				///<Is the left button currently clicked?
 //		float m_cursorPrevX = 0;						///<Previous X position of cursor
 //		float m_cursorPrevY = 0;						///<Previous Y position of cursor
 //		bool  m_makeScreenshot = false;					///<Should I make a screeshot after frame is done?
 //		bool  m_makeScreenshotDepth = false;			///<Should I make a screeshot after frame is done?
-		uint32_t m_numScreenshot = 0;					///<Screenshot ID
+        uint32_t m_numScreenshot = 0;                    ///<Screenshot ID
+        const uint32_t m_max_screenshot_per_sec = 5;
+        uint32_t m_screenshot_current_second = 0;
+        float m_time_sum = 0;
 
-		void onFrameEnded(veEvent event) override;
+        void onFrameEnded(veEvent event) override;
 //		virtual bool onKeyboard(veEvent event);
 //		virtual bool onMouseMove(veEvent event);
 //		virtual bool onMouseButton(veEvent event);
 //		virtual bool onMouseScroll(veEvent event);
 
-	public:
-		///Constructor
-        VEEventListenerCustom( std::string name ) : VEEventListener(name) { };
-		///Destructor
-		~VEEventListenerCustom() override {};
-	};
+    public:
+        ///Constructor
+        VEEventListenerCustom(std::string name) : VEEventListener(name) {};
+
+        ///Destructor
+        ~VEEventListenerCustom() override {};
+    };
 }
 
 
